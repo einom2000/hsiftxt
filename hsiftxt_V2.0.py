7a193c8cbeb574bef3a47ad5fe4448196b50b29e
einom.ng@gmail.com <starBB002347>
2018-12-13 00:26:45 +0800

    11111

--- hsiftxt_V2.0.py
+++ hsiftxt_V2.0.py
@@ -22,16 +22,16 @@ STOP_KEY = 'F12'
 TIME_TO_RUN = 480
 ANTI_AFT_TIME = 10
 
-def getRandomWait(min, max):
+def get_random_wait(low, high):
     # wait for a random time
-    time.sleep(random.randint(min, max) / 1000)
+    time.sleep(random.randint(low, high) / 1000)
 
-def blurPosDur():
+def blur_pos_dur():
     # get a random blur relevant x and y and random time according to the constants
-    blurXplus = random.randint(BLUR_PIXEL[0], BLUR_PIXEL[1]) * random.choice([-1, 1])
-    blurYplus = random.randint(BLUR_PIXEL[0], BLUR_PIXEL[1]) * random.choice([-1, 1])
-    blurTime = random.randint(BLUR_DUR[0], BLUR_DUR[1])
-    return (blurXplus, blurYplus, blurTime)
+    x = random.randint(BLUR_PIXEL[0], BLUR_PIXEL[1]) * random.choice([-1, 1])
+    y = random.randint(BLUR_PIXEL[0], BLUR_PIXEL[1]) * random.choice([-1, 1])
+    t = random.randint(BLUR_DUR[0], BLUR_DUR[1])
+    return x, y, t
 
 
 # Game variables
@@ -41,30 +41,35 @@ soundMissed = 0
 count = 0
 
 # load standard images
-dobberImgs = []
+dobber_images = []
 for i in range(1, 10+1):
-    dobberImgs.append("pp{}.png".format(i))
+    dobber_images.append("pp{}.png".format(i))
 
 
 class CastPole:
     # cast the pole and looking for the dobber
     # move mouse to the dobber
 
-    def __init__(self, mousePosition):
-        self.mousePosition = mousePosition
+    def __init__(self, mouse_position):
+        self.mouse_pos = mouse_position
 
     def cast(self):
         # get a blur
-        blur = blurPosDur()
-        self.mousePosition =tuple(map(lambda x, y: x + y, pyautogui.position(),
-                                      (blur[0] * 3, blur[1] * 3)))
+        blur_x, blur_y, dur_t = blur_pos_dur()
+        self.mouse_pos =tuple(map(lambda x, y: x + y, self.mouse_pos,
+                                      (blur_x * 3, blur_y * 3)))
         # right double click
-        pyautogui.rightClick(self.mousePosition[0], self.mousePosition[1], blur[2])
-        getRandomWait(100, 300)
-        pyautogui.rightClick(self.mousePosition[0], self.mousePosition[1], blur[2])
-        getRandomWait(500, 800)
+        pyautogui.rightClick(self.mouse_pos[0], self.mouse_pos[1], dur_t)
+        get_random_wait(100, 300)
+        pyautogui.rightClick(self.mouse_pos[0], self.mouse_pos[1], dur_t)
+        get_random_wait(500, 800)
 
-    def findhooker(self):
+    def find_hooker(self, rect, confi):
+        found_hook = False
+        for i in range(0, 5):
+            for img in dobber_images:
+                img_found = pyautogui.locateCenterOnScreen()
+
 
 
 
