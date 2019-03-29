62675d14f23b5842cd5ac8e12c47ab19d91a25cc
einom2000 <einom.ng@gmail.com>
2019-03-29 09:34:01 +0800

    1111

--- aucbt_asus_K45V.py
+++ aucbt_asus_K45V.py
@@ -15,6 +15,8 @@ import pyperclip
 import json
 import datetime
 import pyttsx3
+from win32api import GetKeyState
+from win32con import VK_CAPITAL
 
 # 1280*720
 
@@ -354,6 +356,81 @@ def anti_afk():
         scan_is_end('000')
 
 
+while GetKeyState(VK_CAPITAL):
+    pyautogui.press('capslock')
+
+
+def click_hb_btn(btn_name):
+    time.sleep(0.3)
+    pyautogui.moveTo(btn_name[0], btn_name[1], 0.5)
+    time.sleep(0.2)
+    pyautogui.click()
+
+
+def kill_process(process_name, wd_name):
+    for proc in psutil.process_iter():
+        # check whether the process name matches
+        if proc.name() == process_name:
+            proc.kill()
+            break
+    while win32gui.FindWindow(None, wd_name):
+        pass
+    return
+
+
+class LoginWindow:
+
+    windowHwnd = 0
+
+    def __init__(self, programdir, windowname, username, userpwd):
+        self.programDir = programdir
+        self.windowName = windowname
+        self.userName = username
+        self.userPwd = userpwd
+
+    def runbnet(self):
+        exist = win32gui.FindWindow(None, self.windowName)
+        if exist == 0:
+            win32api.WinExec(self.programDir)
+        return
+
+    def findWindow(self):
+        while True:
+            hwndbnt = win32gui.FindWindow(None, self.windowName)
+            if hwndbnt == 0:
+                continue
+            else:
+                win32gui.MoveWindow(hwndbnt, 100, 100, 365, 541, True)
+            break
+        win32gui.SetForegroundWindow(hwndbnt)
+        time.sleep(0.5)
+        return hwndbnt
+
+    def login(self):
+        # to log in id
+        for i in range(4): # orginal 4
+            pyautogui.press('tab')
+            time.sleep(random.randint(3, 5) / 10)
+        # clear box
+        pyautogui.press('backspace')
+        time.sleep(random.randint(3, 5) / 10)
+        # change to english
+        pyautogui.press('shift')
+        time.sleep(random.randint(3, 5) / 10)
+        win32api.LoadKeyboardLayout('00000409', 1)
+        time.sleep(random.randint(3, 5) / 10)
+        # typein
+        pyautogui.typewrite(self.userName, interval=(random.randint(15, 30) / 100))
+        time.sleep((random.randint(15, 30) / 100))
+        pyautogui.press('tab')
+        pyautogui.typewrite(self.userPwd, interval=(random.randint(15, 30) / 100))
+        time.sleep(5)
+        for i in range(3):
+            pyautogui.press('tab')
+            time.sleep(random.randint(3, 5) / 10)
+        # log in
+        pyautogui.press('enter')
+        return
 
 # ======CONSTANTS========
 '''
